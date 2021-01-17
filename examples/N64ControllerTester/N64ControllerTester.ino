5ba0947150389beae7c0ad857ade575ac7cbaa33
GitModu <batatas@gmail.com>
2021-01-17 19:12:49 +0000

    Updated example to be able to test both solutions.

--- examples/N64ControllerTester/N64ControllerTester.ino
+++ examples/N64ControllerTester/N64ControllerTester.ino
@@ -1,11 +1,16 @@
-#include <NintendoControllerReaderSTM32.h>
+#include <BitBangN64Controller.h>
+#include <SerialJoyBusN64Controller.h>
 
 const uint32_t SERIAL_BAUD_RATE = 115200;
 const uint32_t CONTROLLER_PIN = PA0;
 
+Nintendo64::Data_t RawData;
 
-N64Data_t RawData;
-N64Controller Controller(CONTROLLER_PIN);
+BitBangN64Controller BitBangN64(CONTROLLER_PIN);
+
+HardwareSerial* SerialInstance = &Serial3;
+
+SerialJoyN64Controller JoyBusN64(SerialInstance);
 
 void setup()
 {
@@ -13,26 +18,76 @@ void setup()
 	while (!Serial)
 		;
 	delay(1000);
-	Serial.println(F("Nintendo 64 Controller Tester"));
 
+	// Serial Joy requires a start to initialize the serial hardware.
+	JoyBusN64.Start();
+
+	Serial.println(F("Nintendo 64 Controller Tester"));
+}
+
+const bool SerialJoyLoop()
+{
+	// Poll can be sent and response read later, asynchronously.
+	JoyBusN64.Poll();
+	delay(1); // Here we just use delay for example.
+	if (JoyBusN64.Read())
+	{
+		RawData.Buttons = JoyBusN64.Data.Buttons;
+		RawData.JoystickX = JoyBusN64.Data.JoystickX;
+		RawData.JoystickY = JoyBusN64.Data.JoystickY;
+
+		return true;
+	}
+	else
+	{
+		RawData.Buttons = 0;
+		RawData.JoystickX = 0;
+		RawData.JoystickY = 0;
+
+		return false;
+	}
+}
+
+const bool BitBangJoyLoop()
+{
+	if (BitBangN64.Poll())
+	{
+		RawData.Buttons = BitBangN64.Data.Buttons;
+		RawData.JoystickX = BitBangN64.Data.JoystickX;
+		RawData.JoystickY = BitBangN64.Data.JoystickY;
+
+		return true;
+	}
+	else
+	{
+		RawData.Buttons = 0;
+		RawData.JoystickX = 0;
+		RawData.JoystickY = 0;
+
+
+		return false;
+	}
 }
 
 void loop()
 {
-	if (Controller.Read(&RawData))
+	if (SerialJoyLoop())
+	//if (BitBangJoyLoop())
 	{
-		for (uint8_t i = 0; i < (sizeof(RawData.Buttons) * 8); i++)
+		Serial.print(F("Buttons: 0b"));
+		for (uint8_t i = 0; i < sizeof(RawData.Buttons) * 8; i++)
 		{
-			Serial.print((RawData.Buttons & (1 << i)) >> i);
+			Serial.print((RawData.Buttons >> i) & 0b1);
 		}
-
-		Serial.print('\t');
+		Serial.print(F("\tJoystick: "));
 		Serial.print(RawData.JoystickX);
 		Serial.print('\t');
 		Serial.print(RawData.JoystickY);
 		Serial.println();
 
-		delay(200);
+		Serial.println();
+
+		delay(100);
 	}
 	else
 	{
